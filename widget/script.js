e1c29a495e7844188c17240b2b1892e44922edce
Glucose md <C:\Users\gluco\AppData\Roaming\The Bat!>
2023-06-06 12:33:26 +0300

    Update script.js

--- widget/script.js
+++ widget/script.js
@@ -1,103 +1,113 @@
-var MyWidget = function() {
-    var self = this;
+window.reactBaseWidget = {};
+window.reactBaseWidget.sourceUrl = 'http://localhost:8081/';
+window.reactBaseWidget.productSysname = 'reactBaseWidget';
 
-    this.callbacks = {
-        render: function() {
-            console.log('render');
+define([
+    "jquery",
+    "lib/components/base/modal",
+    window.reactBaseWidget.sourceUrl + 'main.bundle.js',
+], function($, Modal, mainBundle) {
+    const Widget = function () {
 
-            // Crearea butonului
-            let btn = document.createElement("button");
+        const self = this;
+        self.system = this.system();
+        self.langs = this.langs;
 
-            // Setarea stilului butonului
-            btn.style.width = "65px";
-            btn.style.height = "54px";
-            btn.innerHTML = "Click";
+        const App = window.appWidget;
 
-            // Atasarea functiei de click la buton
-            btn.onclick = function() {
-                // Crearea ferestrei popup
-                let popup = document.createElement("div");
+        this.getTemplate = function(callback) {
+            var template = '<button id="myButton" style="width:65px;height:54px;">Click</button>';
+            callback(template);
+        };
 
-                // Setarea stilului ferestrei popup
-                popup.style.width = "80%";
-                popup.style.height = "80%";
-                popup.style.position = "fixed";
-                popup.style.top = "10%";
-                popup.style.left = "10%";
-                popup.style.background = "white";
-                popup.style.zIndex = "1000";
-                popup.style.padding = "20px";
-                popup.style.boxShadow = "0px 0px 10px rgba(0,0,0,0.5)";
-                popup.style.textAlign = "center";
+        this.renderModal = function() {
+            console.log('Succes');
+            self.getTemplate(function(template) {
+                var $modal = new Modal({
+                    class_name: 'modal-window',
+                    init: function ($modal_body) {
+                        var $this = $(this);
+                        $modal_body
+                            .trigger('modal:loaded')
+                            .html(template)
+                            .trigger('modal:centrify')
+                            .append('<span class="modal-body__close"><span class="icon icon-modal-close"></span></span>');
 
-                // Adăugarea textului la fereastra popup
-                popup.innerHTML = "<h1>Salut</h1>";
+                        $("#myButton").click(function() {
+                            let popup = document.createElement("div");
+                            popup.style.width = "80%";
+                            popup.style.height = "80%";
+                            popup.style.position = "fixed";
+                            popup.style.top = "10%";
+                            popup.style.left = "10%";
+                            popup.style.background = "white";
+                            popup.style.zIndex = "1000";
+                            popup.style.padding = "20px";
+                            popup.style.boxShadow = "0px 0px 10px rgba(0,0,0,0.5)";
+                            popup.style.textAlign = "center";
+                            popup.innerHTML = "<h1>Salut</h1>";
+                            document.body.appendChild(popup);
 
-                // Adăugarea ferestrei popup la body
-                document.body.appendChild(popup);
+                            let closeBtn = document.createElement("button");
+                            closeBtn.innerHTML = "Închide";
+                            closeBtn.style.position = "absolute";
+                            closeBtn.style.right = "20px";
+                            closeBtn.style.top = "20px";
+                            closeBtn.onclick = function() {
+                                document.body.removeChild(popup);
+                            };
+                            popup.appendChild(closeBtn);
+                        });
+                    },
+                    destroy: function () {}
+                });
+            });
+        };
 
-                // Crearea unui buton de închidere pentru fereastra popup
-                let closeBtn = document.createElement("button");
-                closeBtn.innerHTML = "Închide";
-                closeBtn.style.position = "absolute";
-                closeBtn.style.right = "20px";
-                closeBtn.style.top = "20px";
-
-                // Atasarea funcției de click la butonul de închidere
-                closeBtn.onclick = function() {
-                    document.body.removeChild(popup);
-                };
-
-                // Adăugarea butonului de închidere la fereastra popup
-                popup.appendChild(closeBtn);
-            };
-
-            // Găsirea locației pentru inserarea butonului
-            let buttonLocation = document.querySelector("#nav_menu > div:nth-child(12)");
-
-            // Adăugarea butonului sub locația dorită
-            buttonLocation.insertAdjacentElement('afterend', btn);
-
-            return true;
-        },
-        init: function() {
-            console.log('init');
-            return true;
-        },
-        bind_actions: function() {
-            console.log('bind_actions');
-            return true;
-        },
-        settings: function() {
-            console.log('settings');
-            return true;
-        },
-        onSave: function() {
-            console.log('onSave');
-            return true;
-        },
-        destroy: function() {
-            console.log('destroy');
-            return true;
-        },
-        contacts: {
-            //select contacts in list and clicked on widget name
-            selected: function() {
-                console.log('contacts');
+        this.callbacks = {
+            render: function() {
+                console.log('render');
+                App.render(self);
+                self.renderModal();
+                return true;
+            },
+            init: function () {
+                console.log('init');
+                App.init(self);
+                return true;
+            },
+            bind_actions: function () {
+                App.bind_actions(self);
+                return true;
+            },
+            settings: function () {
+                console.log('settings');
+                App.settings(self);
+                return true;
+            },
+            dpSettings: function () {
+                console.log('dpSettings');
+                App.dpSettings(self);
+            },
+            advancedSettings: function () {
+                console.log('advancedSettings');
+                App.advancedSettings(self);
+                return true;
+            },
+            onSave: function () {
+                App.onSave(self);
+                return true;
+            },
+            destroy: function() {
+                App.destroy(self);
+                return true;
             }
-        },
-        leads: {
-            //select leads in list and clicked on widget name
-            selected: function() {
-                console.log('leads');
-            }
-        },
-        tasks: {
-            //select taks in list and clicked on widget name
-            selected: function() {
-                console.log('tasks');
-            }
-        }
+        };
+
+        return this;
+
     };
-    return this;
-};
+
+    return Widget;
+
+});
